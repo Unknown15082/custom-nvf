c77d71eb09cd34c714e4f98f4939501d1ff90410
Unknown15082 <trangiahuy15082006@gmail.com>
2025-01-07 22:14:32 +0700

    Treesitter

--- configs/default.nix
+++ configs/default.nix
@@ -2,5 +2,6 @@
 	imports = [
 		./general.nix
 		./options.nix
+		./treesitter.nix
 	];
 }
---
+++ configs/treesitter.nix
@@ -0,0 +1,15 @@
+{
+	config.vim = {
+		treesitter = {
+			enable = true;
+			addDefaultGrammars = true;
+
+			context.enable = true;
+			highlight.enable = true;
+			incrementalSelection.enable = true;
+			indent.enable = true;
+		};
+
+		languages.enableTreesitter = true;
+	};
+}
