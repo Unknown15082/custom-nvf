35e6502e96108e6d3a536637a7293fee9bfcbcfd
Unknown15082 <trangiahuy15082006@gmail.com>
2025-04-02 02:00:57 +0800

    Keymaps

---
+++ configs/keymaps.nix
@@ -0,0 +1,22 @@
+{
+	config.vim.keymaps = [
+		{ # Clear highlight
+			key = "<Esc>";
+			mode = "n";
+			silent = true;
+			action = "<Cmd>noh<CR>";
+		}
+		{ # Visual gj
+			key = "j";
+			mode = [ "n" "x" ];
+			silent = true;
+			action = "v:count == 0 ? 'gj' : 'j'";
+		}
+		{ # Visual gk
+			key = "k";
+			mode = [ "n" "x" ];
+			silent = true;
+			action = "v:count == 0 ? 'gk' : 'k'";
+		}
+	];
+}
